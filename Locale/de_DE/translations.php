cbba61067657c891883f2a6257a574feaf1012c0
Timo <timo.litzbarski@gmail.com>
2019-08-09 09:58:17 +0200

    german translation

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'Milestone' => 'Meilenstein',
+    'The Milestone Plugin for Kanboard adds a section for milestones to show their related tasks.' => 'Das Milestone Plugin für Kanboard fügt einen Abschnitt für Meilensteine hinzu, um die zugehörigen Aufgaben anzuzeigen.',
+    'Total progress' => 'Gesamter Fortschritt',
+    'Total time tracking' => 'Gesamtzeiterfassung',
+    'remaining' => 'verblieben',
+    'Edit the internal link' => 'Bearbeiten des internen Links'
+);

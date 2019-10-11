60e7fa937c16ca29b25ec5ebaf7666b75e99d281
Litzbarski <Timo.Litzbarski@ottogroup.com>
2019-10-11 17:29:05 +0200

    fix milestone detection for renamed links

--- Template/task_internal_link/show.php
+++ Template/task_internal_link/show.php
@@ -1,6 +1,6 @@
-<?php if (!empty($links['is a milestone of'])): ?>
+<?php if (!empty($links['is a milestone of']) || isset($link_label_list) && isset($link_label_list[9]) && !empty($links[$link_label_list[9]])): ?>
 <?= $this->render('milestone/show', array(
-    'milestone' => $links['is a milestone of'],
+    'milestone' => !empty($links['is a milestone of']) ? $links['is a milestone of']: $links[$link_label_list[9]],
     'task' => $task,
     'project' => $project,
     'editable' => $editable,
@@ -8,6 +8,7 @@
     'link_label_list' => isset($link_label_list) ? $link_label_list : array(),
 )) ?>
 <?php unset($links['is a milestone of']); ?>
+<?php if (isset($link_label_list) && isset($link_label_list[9])): unset($links[$link_label_list[9]]); endif ?>
 <?php endif ?>
 
 <?= $this->render('kanboard:task_internal_link/show', array(

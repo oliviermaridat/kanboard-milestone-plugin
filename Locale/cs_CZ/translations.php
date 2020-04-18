64dd6cae6823b74aceaac91abdef43e8fcfc1049
trendspotter <j.podhorecky@volny.cz>
2020-04-18 18:06:45 +0200

    Add cs_CZ translations

---
+++ Locale/cs_CZ/translations.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'Milestone' => 'Milník',
+    'The Milestone Plugin for Kanboard adds a section for milestones to show their related tasks.' => 'Milestone Plugin pro Kanboard přidává sekci pro milníky, aby ukázala související úkoly.',
+    'Total progress' => 'Celkový pokrok',
+    'Total time tracking' => 'Sledování celkového času',
+    'remaining' => 'zbývající',
+    'Edit the internal link' => 'Upravit interní odkaz'
+);

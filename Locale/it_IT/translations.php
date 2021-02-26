0639a56df32848aa89238109628b3c3f92c2df65
Kenlog <valentino@iltuobrand.it>
2021-02-26 19:19:35 +0100

    Add Italian translation is sentence not included

---
+++ Locale/it_IT/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Milestone' => 'Milestone',
+    'The Milestone Plugin for Kanboard adds a section for milestones to show their related tasks.' => 'Il plug-in Milestone per Kanboard aggiunge una sezione per le pietre miliari per mostrare le loro attività correlate.',
+    'Total progress' => 'Progresso totale',
+    'Total time tracking' => 'Monitoraggio del tempo totale',
+    'remaining' => 'residuo',
+    'Edit the internal link' => 'Modifica il collegamento interno',
+	'Add a new task into this milestone' => 'Aggiungi una nuova attività a questo Milestone',
+];
